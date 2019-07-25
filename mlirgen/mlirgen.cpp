2fe1df58bb4b15ce410da13b79a2c8ba159c8a90
chethanr <chethan.vasishta@gmail.com>
2019-07-25 15:13:49 +0530

    Adding a simple MLIRGen example to understand the APIs

---
+++ mlirgen/mlirgen.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <mlir/IR/MLIRContext.h>
+using namespace std;
+using namespace mlir;
+
+int main()
+{
+	cout <<"Simple program to write a DSL with MLIR" <<endl;
+	MLIRContext mlirContext;
+	return 0;
+}
