f3b50905136a379244fd6939f59b43a52fb9a923
cr <chethan.vasishta@gmail.com>
2019-07-28 13:41:43 +0530

    Added a main function to call the add function, and a way to convert to llvm IR. Conversion to llvm IR is yet to work

--- mlirgen/mlirgen.cpp
+++ mlirgen/mlirgen.cpp
@@ -5,13 +5,43 @@
 #include <mlir/IR/StandardTypes.h>
 #include <mlir/IR/Builders.h>
 #include <mlir/EDSC/Intrinsics.h>
-
+#include <mlir/Support/LLVM.h> // SmallVector
+#include <mlir/Target/LLVMIR/ModuleTranslation.h>
 using namespace std;
+
+void CreateMainForAddTwoNumbers(mlir::MLIRContext& mlirContext, mlir::OwningModuleRef& module)
+{
+    llvm::SmallVector<mlir::Type, 0> emptyArgs;
+    mlir::IntegerType i32Type = mlir::IntegerType::get(32, &mlirContext);
+    auto mainFuncType = mlir::FunctionType::get(emptyArgs, i32Type, &mlirContext);
+
+    // create the main function
+    mlir::FuncOp mainFunction = mlir::FuncOp::create(mlir::UnknownLoc::get(&mlirContext), "main", mainFuncType, {});
+    mainFunction.addEntryBlock();
+    auto builder = llvm::make_unique<mlir::OpBuilder>(mainFunction.getBody());
+
+    // generate constants
+    auto valueFive = builder->create<mlir::ConstantOp>(mlir::UnknownLoc::get(&mlirContext), mlir::IntegerAttr::get(i32Type, 5));
+    auto valueTen = builder->create<mlir::ConstantOp>(mlir::UnknownLoc::get(&mlirContext), mlir::IntegerAttr::get(i32Type, 10));
+
+    // call 'AddTwoInt32'
+    llvm::SmallVector<mlir::Value*, 2> args { valueFive, valueTen };
+    // auto result = builder->create<mlir::GenericCallOp>(mlir::UnknownLoc::get(&mlirContext), "AddTwoInt32", args); 
+
+    // add the function to the module
+    // module->push_back(mainFunction);
+}
+
+void TranslateModuleToLLVM(const mlir::ModuleOp& module)
+{
+    auto llvmModule = mlir::LLVM::ModuleTranslation::translateModule<>(module);
+    // llvmModule->dump();
+}
 
 void TestAddTwoNumbers()
 {
-	mlir::MLIRContext mlirContext;
-	mlir::OwningModuleRef module = mlir::ModuleOp::create(mlir::UnknownLoc::get(&mlirContext));    
+    mlir::MLIRContext mlirContext;
+    mlir::OwningModuleRef module = mlir::ModuleOp::create(mlir::UnknownLoc::get(&mlirContext));    
 
     // create the function prototype    
     mlir::IntegerType i32Type = mlir::IntegerType::get(32, &mlirContext);
@@ -19,7 +49,7 @@ void TestAddTwoNumbers()
     auto func_type = mlir::FunctionType::get(arg_types, i32Type, &mlirContext);
 
     // create the function
-	mlir::FuncOp addFunction = mlir::FuncOp::create(mlir::UnknownLoc::get(&mlirContext), "Add", func_type, {}/* attrs*/);;
+    mlir::FuncOp addFunction = mlir::FuncOp::create(mlir::UnknownLoc::get(&mlirContext), "AddTwoInt32", func_type, {}/* attrs*/);;
     addFunction.addEntryBlock();
     auto builder = llvm::make_unique<mlir::OpBuilder>(addFunction.getBody());
 	
@@ -33,7 +63,15 @@ void TestAddTwoNumbers()
     // add function into the module
     module->push_back(addFunction);
 
+    // create a main function
+    CreateMainForAddTwoNumbers(mlirContext, module);
+
+    // dump the MLIR
+    cout << "Generated MLIR: " << endl;
     module->dump();
+
+    // convert to LLVM
+    TranslateModuleToLLVM(module.get());
 }
 
 int main()
